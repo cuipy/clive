69661193005320b8cf2e02fe767144a034dcee10
cuipengyu <cuipy2004@163.com>
2018-09-14 11:08:42 +0800

    刷新缓存

--- public/static/customer/js/kefu.js
+++ public/static/customer/js/kefu.js
@@ -1,3 +1,3 @@
 
 var dqurl='http://clive.pingbuwang.com';
-document.write("<script src="+dqurl+"/static/customer/js/layer/layer.js></script><script src="+dqurl+"/static/customer/js/clive-tool.js></script><script src="+dqurl+"/static/customer/js/clive-ys.js></script>");
+document.write("<script src="+dqurl+"/static/customer/js/layer/layer.js></script><script src="+dqurl+"/static/customer/js/clive-tool.js></script><script src="+dqurl+"/static/customer/js/clive-ys.js?v=1></script>");
